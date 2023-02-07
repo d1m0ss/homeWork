6b8fb3905be98a9b187459ccb786cf0882c1ed76
Dmitriy <Dima.volkov1998@gmail.com>
2023-02-07 16:16:52 +0300

    addedSomeScripts
    And made some buttons work

--- JavaScript/Todo-list/script.js
+++ JavaScript/Todo-list/script.js
@@ -1,7 +1,7 @@
 const headerRowTop = document.createElement("section");
 const toDoHeader = document.createElement("header");
 const listButton = document.createElement("button");
-const listWrapper = document.createElement("label");
+const listWrapper = document.createElement("article");
 const toDoBody = document.createElement("section");
 const listCount = document.createElement("span");
 const topInput = document.createElement("input");
@@ -13,6 +13,7 @@ const root = document.getElementById("root");
 
 const completedCount = 0;
 let allCount = 2;
+const date = new Date();
 
 const listCountCompleted = listCount.cloneNode(true);
 const listCountAll = listCount.cloneNode(true);
@@ -34,29 +35,69 @@ const bottomInput = topInput.cloneNode(true);
 
 bottomInput.setAttribute("placeholder", "Search ...");
 topInput.setAttribute("placeholder", "Enter todo ...");
+listCountAll.setAttribute("id", "countAll");
 listButton.setAttribute("type", "submit");
 bottomInput.setAttribute("type", "text");
 listCB.setAttribute("type", "checkbox");
 topInput.setAttribute("type", "text");
+listWrapper.setAttribute("id", "rootWrapper");
 
 const bottomButtonShowCompleted = listButton.cloneNode(true);
 const bottomButtonShowAll = listButton.cloneNode(true);
-const topButtondeleteLast = listButton.cloneNode(true);
+const topButtonDeleteLast = listButton.cloneNode(true);
 const topButtonDeleteAll = listButton.cloneNode(true);
 const topButtonAdd = listButton.cloneNode(true);
 
 listCountCompleted.innerText = `Completed: ${completedCount}`;
 bottomButtonShowCompleted.innerText = "Show Comleted";
-topButtondeleteLast.innerText = "Delete Last";
+topButtonDeleteLast.innerText = "Delete Last";
 topButtonDeleteAll.innerText = "Delete All";
 listCountAll.innerText = `All: ${allCount}`;
 bottomButtonShowAll.innerText = "Show All";
 topButtonAdd.innerText = "Add";
-listDate.innerText = "Date";
+listDate.innerText = date.toLocaleString();
 listText.innerText = "ToDo Text";
 listButton.innerText = "X";
 
 const headerRowBottom = headerRowTop.cloneNode(true);
+
+const totalCount = () => {
+  const count = document.getElementById("countAll");
+  count.innerText = `All: ${toDoBody.children.length}`;
+};
+
+const todoAdd = () => {
+  if (topInput.value === "") {
+    topInput.focus()
+  } else {
+    topInput.value === ""
+      ? (listText.innerText = "ToDo Text")
+      : (listText.innerText = `${topInput.value}`);
+    const date = new Date();
+    listDate.innerText = date.toLocaleString();
+    const listWrapperClone = listWrapper.cloneNode(true);
+    toDoBody.append(listWrapperClone);
+    topInput.value = "";
+    totalCount();
+  }
+};
+
+const todoDeleteLast = () => {
+  toDoBody.children[toDoBody.childElementCount - 1].remove();
+  totalCount();
+};
+
+const todoDeleteAll = () => {
+  for (let i = toDoBody.childElementCount; i > 0; i--) {
+    toDoBody.children[0].remove();
+  }
+  totalCount();
+};
+
+topButtonDeleteLast.addEventListener("click", todoDeleteLast);
+topButtonDeleteAll.addEventListener("click", todoDeleteAll);
+listCountAll.addEventListener("click", totalCount);
+topButtonAdd.addEventListener("click", todoAdd);
 
 headerRowBottom.append(
   listCountAll,
@@ -67,10 +108,11 @@ headerRowBottom.append(
 );
 headerRowTop.append(
   topButtonDeleteAll,
-  topButtondeleteLast,
+  topButtonDeleteLast,
   topInput,
   topButtonAdd
 );
+
 toDoHeader.append(headerRowTop, headerRowBottom);
 listWrapper.append(listCB, listText, listBox);
 listBox.append(listButton, listDate);
