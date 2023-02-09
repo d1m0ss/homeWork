836db9d465c0f5694029d41cce8b90a0679adb6a
Dmitriy <Dima.volkov1998@gmail.com>
2023-02-09 16:25:48 +0300

    comleteSomeEvents

--- JavaScript/Todo-list/script.js
+++ JavaScript/Todo-list/script.js
@@ -1,74 +1,97 @@
-const headerRowTop = document.createElement("section");
-const toDoHeader = document.createElement("header");
-const listButton = document.createElement("button");
-const listWrapper = document.createElement("article");
-const toDoBody = document.createElement("section");
-const listCount = document.createElement("span");
-const topInput = document.createElement("input");
-const listText = document.createElement("span");
-const listDate = document.createElement("span");
-const listCB = document.createElement("input");
-const listBox = document.createElement("div");
+const listButton = createElement("button", {
+  className: "root__button",
+  textContent: "X",
+  type: "submit",
+});
+const toDoBody = createElement("section", {
+  className: "root__body",
+  id: "todoBody",
+});
+const listText = createElement("span", {
+  className: "root__text",
+  textContent: "Todo Text",
+});
+const topInput = createElement("input", {
+  className: "root__input",
+  placeholder: "Enter todo ...",
+});
+const listCB = createElement("input", {
+  className: "root__check-box",
+  type: "checkbox",
+});
+const headerRowTop = createElement("section", {
+  className: "root__header-row",
+});
+const listWrapper = createElement("article", { className: "root__wrapper" });
+const listBox = createElement("div", { className: "root__item-right-bar" });
+const toDoHeader = createElement("header", { classnName: "root__header" });
+const listDate = createElement("span", { className: "root__date" });
 const root = document.getElementById("root");
+const listCount = createElement("span");
 
-const completedCount = 0;
+let completeCount = 0;
 let allCount = 2;
 const date = new Date();
 
 const listCountCompleted = listCount.cloneNode(true);
 const listCountAll = listCount.cloneNode(true);
-
-listCountCompleted.classList = "root__count-completed";
-headerRowTop.classList = "root__header-row";
-listCountAll.classList = "root__count-all";
-listBox.classList = "root__item-right-bar";
-listWrapper.classList = "root__wrapper";
-toDoHeader.classList = "root__header";
-listButton.classList = "root__button";
-listCB.classList = "root__check-box";
-listText.classList = "root__text";
-listDate.classList = "root__date";
-topInput.classList = "root__input";
-toDoBody.classList = "root__body";
-
 const bottomInput = topInput.cloneNode(true);
 
-bottomInput.setAttribute("placeholder", "Search ...");
-topInput.setAttribute("placeholder", "Enter todo ...");
-listCountAll.setAttribute("id", "countAll");
-listButton.setAttribute("type", "submit");
-bottomInput.setAttribute("type", "text");
-listCB.setAttribute("type", "checkbox");
-topInput.setAttribute("type", "text");
-listWrapper.setAttribute("id", "rootWrapper");
+listCountCompleted.classList = "root__count-completed";
+listCountAll.classList = "root__count-all";
 
 const bottomButtonShowCompleted = listButton.cloneNode(true);
 const bottomButtonShowAll = listButton.cloneNode(true);
 const topButtonDeleteLast = listButton.cloneNode(true);
 const topButtonDeleteAll = listButton.cloneNode(true);
+const headerRowBottom = headerRowTop.cloneNode(true);
 const topButtonAdd = listButton.cloneNode(true);
 
-listCountCompleted.innerText = `Completed: ${completedCount}`;
+bottomInput.setAttribute("placeholder", "Search ...");
+listCountCompleted.setAttribute("id", "completeCount");
+listButton.setAttribute("id", "showAllButton");
+listCountAll.setAttribute("id", "countAll");
+
+listCountCompleted.innerText = `Completed: ${completeCount}`;
 bottomButtonShowCompleted.innerText = "Show Comleted";
 topButtonDeleteLast.innerText = "Delete Last";
 topButtonDeleteAll.innerText = "Delete All";
 listCountAll.innerText = `All: ${allCount}`;
+listDate.innerText = date.toLocaleString();
 bottomButtonShowAll.innerText = "Show All";
 topButtonAdd.innerText = "Add";
-listDate.innerText = date.toLocaleString();
-listText.innerText = "ToDo Text";
-listButton.innerText = "X";
 
-const headerRowBottom = headerRowTop.cloneNode(true);
+function createElement(tagName, options) {
+  const element = Object.assign(document.createElement(tagName), options);
+  return element;
+}
 
-const totalCount = () => {
+function totalCount() {
   const count = document.getElementById("countAll");
   count.innerText = `All: ${toDoBody.children.length}`;
-};
+}
+
+function showAllBtn() {
+  const allElement = todoBody.querySelectorAll(".hiden");
+  allElement.forEach((item) => {
+    item.style.display === "none"
+      ? (item.style.display = "flex")
+      : (item.style.display = "none");
+  });
+  console.log(allElement);
+  completedCount();
+  totalCount();
+}
+
+function completedCount() {
+  const count = document.getElementById("completeCount");
+  const quantity = todoBody.querySelectorAll(".hiden");
+  count.innerText = `Completed: ${quantity.length}`;
+}
 
 const todoAdd = () => {
   if (topInput.value === "") {
-    topInput.focus()
+    topInput.focus();
   } else {
     topInput.value === ""
       ? (listText.innerText = "ToDo Text")
@@ -82,9 +105,15 @@ const todoAdd = () => {
   }
 };
 
+const todoAddAtKey = (e) => {
+  if (e.keyCode !== 13) return;
+  todoAdd();
+};
+
 const todoDeleteLast = () => {
   toDoBody.children[toDoBody.childElementCount - 1].remove();
   totalCount();
+  completedCount();
 };
 
 const todoDeleteAll = () => {
@@ -92,12 +121,14 @@ const todoDeleteAll = () => {
     toDoBody.children[0].remove();
   }
   totalCount();
+  completedCount();
 };
 
 topButtonDeleteLast.addEventListener("click", todoDeleteLast);
 topButtonDeleteAll.addEventListener("click", todoDeleteAll);
-listCountAll.addEventListener("click", totalCount);
 topButtonAdd.addEventListener("click", todoAdd);
+bottomButtonShowAll.addEventListener("click", showAllBtn);
+topInput.addEventListener("keydown", todoAddAtKey);
 
 headerRowBottom.append(
   listCountAll,
@@ -122,5 +153,66 @@ root.append(toDoHeader, toDoBody);
 for (let i = 0; i < allCount; ++i) {
   const listWrapperClone = listWrapper.cloneNode(true);
   toDoBody.append(listWrapperClone);
-  listText.innerText = `ToDo Text`;
+  listText.innerText = listText.innerText;
 }
+
+const todoBody = document.getElementById("todoBody");
+
+const completeTodo = (element) => {
+  const target = element.target;
+  const todoChecked = target.querySelector(".root__check-box");
+  const todoCheckedText = target.querySelector(".root__text");
+  if (target.className === "root__wrapper hiden") {
+    target.classList.remove("hiden");
+  }
+  if (target.className !== "root__wrapper") return;
+  if (target.style.background === "green") {
+    if (target.children[1] == todoCheckedText) {
+      todoCheckedText.style.textDecoration = "none";
+    }
+    target.style.background = "none";
+    todoChecked.checked = false;
+    completedCount();
+    totalCount();
+  } else {
+    if (target.children[1] == todoCheckedText) {
+      todoCheckedText.style.textDecoration = "line-through";
+    }
+    target.style.background = "green";
+    target.style.display = "none";
+    todoChecked.checked = true;
+    target.classList.add("hiden");
+    completedCount();
+    totalCount();
+  }
+};
+
+const correctTodo = (element) => {
+  const target = element.target;
+  if (target.className !== "root__text") return;
+  const inputElement = createElement("input", {
+    className: "root__input",
+    type: "text",
+    placeholder: "Enter todo text ...",
+  });
+  target.outerHTML = inputElement.outerHTML;
+};
+
+const closeTodo = (elem) => {
+  const target = elem.target;
+  if (target.className !== "root__button") return;
+  elem.target.parentElement.parentElement.remove();
+  completedCount();
+  totalCount();
+};
+
+const todoFilter = () => {
+  const filteredItems = todoBody.childNodes;
+  // console.log(todoBody.filter((item) => {  }));
+  todoBody.childNodes[0].classList.add("hiden");
+};
+
+todoBody.addEventListener("click", closeTodo);
+todoBody.addEventListener("click", completeTodo);
+todoBody.addEventListener("click", correctTodo);
+bottomInput.addEventListener("keydown", todoFilter);
